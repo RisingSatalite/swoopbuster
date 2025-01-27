3ef3b019fbe5053ed81a94df97e629066cd90893
SaihajMann <Saihajmann07@gmail.com>
2025-01-26 21:38:14 -0700

    Update SwoopBuster.py

--- SwoopBuster.py
+++ SwoopBuster.py
@@ -11,6 +11,7 @@ def run_command_line_tool(website, hitList="checklist.txt", results="results.txt
         # Build the command
         command = f"./gobuster/{engine}/gobuster {type} -u https://{website} -w {hitList} -o {results}"
 
+        print(command)
         command_args = command
 
         # Run the command and capture the output
@@ -33,6 +34,10 @@ def run_command_line_tool(website, hitList="checklist.txt", results="results.txt
     except Exception as e:
         print("An error occurred:", str(e))
         return None
+
+def domain_simplify(text):
+    domain = text
+    return domain
 
 class MyApp(App):
     def build(self):
@@ -77,13 +82,63 @@ class MyApp(App):
         layout.add_widget(layoutAttackBar)
 
         # Result added here
-        self.result_label = Label(text='Result will load her')
-        layout.add_widget(self.result_label)
+        #self.result_label = Label(text='Result will load here', size_hint_x=None, size_hint_y=None)
+        #layout.add_widget(self.result_label)
+
+        layoutType = BoxLayout(orientation='horizontal', spacing=10, padding=10)
+
+        # Create the main button
+        self.main_button = Button(text="Select an engine type", size_hint=(None, None), size=(520, 70))
+
+        self.engineDropDown = DropDown()
+        for engineType in ["gobuster_Windows_i386", "gobuster_Windows_x86_64", "gobuster_Windows_arm64", "gobuster_Linux_i386", "gobuster_Linux_arm64", "gobuster_Darwin_x86_64", "gobuster_Darwin_arm64"]:
+        
+            # Adding button in drop down list
+            btn = Button(text=engineType, size_hint_y=None, height=70)
+        
+            # binding the button to show the text when selected
+            btn.bind(on_release=lambda btn: self.on_select_option(btn.text, self.engineDropDown, self.main_button))
+        
+            # then add the button inside the dropdown
+            self.engineDropDown.add_widget(btn)
+
+        # Attach the dropdown to the main button
+        self.main_button.bind(on_release=self.engineDropDown.open)
+
+        self.type_button = Button(text="Scan type", size_hint=(None, None), size=(520, 70))
+
+        self.scanType = DropDown()
+        for engineType in ["dir", "dns"]:
+        
+            # Adding button in drop down list
+            btn = Button(text=engineType, size_hint_y=None, height=70)
+        
+            # binding the button to show the text when selected
+            btn.bind(on_release=lambda btn: self.on_select_option(btn.text, self.scanType, self.type_button))
+        
+            # then add the button inside the dropdown
+            self.scanType.add_widget(btn)
+
+        # Attach the dropdown to the main button
+        self.type_button.bind(on_release=self.scanType.open)
+
+        layoutType.add_widget(self.main_button)
+        layoutType.add_widget(self.type_button)
+
+        layout.add_widget(layoutType)
 
         return layout
 
     def scan(self, instance):
-        self.result_label.text = run_command_line_tool(website=self.input_text.text, hitList=self.attack_list.text, results=self.output_where.text)
+        #self.input_text.text
+        #self.result_label.text = 
+        run_command_line_tool(website=self.input_text.text, hitList=self.attack_list.text, results=self.output_where.text, type=self.type_button.text, engine=self.main_button.text)
+
+    def on_select_option(self, text, dropdown, that_button):
+        # Update the main button text with the selected option
+        that_button.text = f"{text}"
+        # Close the dropdown
+        dropdown.dismiss()
         
 if __name__ == '__main__':
     MyApp().run()
